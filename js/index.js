9ff30bf0d73e8d4edb3367358dda75fbf341ba15
florencia zayas furnari <florenciazayasfurnari@gmail.com>
2022-06-28 18:59:49 -0300

    desafio ciclos

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let entrada = prompt ("Jugamos? Adivina el numero del 1 al 10");
+    while (entrada != "6") {
+        alert(" es incorrecto " + entrada);
+        entrada = prompt("Intenta de nuevo");
+    }
