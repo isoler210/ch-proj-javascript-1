e7dffd9fec9588ceb971c762a4267e0fa923b083
florencia zayas furnari <florenciazayasfurnari@gmail.com>
2022-06-29 00:02:40 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-let entrada = prompt ("Jugamos? Adivina el numero del 1 al 10");
-    while (entrada != "6") {
-        alert(" es incorrecto " + entrada);
-        entrada = prompt("Intenta de nuevo");
-    }
+entrada = prompt ("Jugamos? Adivina el numero del 1 al 10 para poder entrar");
+while (entrada != "6") {
+    alert(" es incorrecto " + entrada);
+    entrada = prompt("Intenta de nuevo");
+}
+
