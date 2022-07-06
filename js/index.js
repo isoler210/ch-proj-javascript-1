d3748d686ec76266342b58716d56390adc3beb20
florencia zayas furnari <florenciazayasfurnari@gmail.com>
2022-07-05 21:56:27 -0300

    segundo desafio

--- js/index.js
+++ js/index.js
@@ -1,6 +1,45 @@
-entrada = prompt ("Jugamos? Adivina el numero del 1 al 10 para poder entrar");
-while (entrada != "6") {
-    alert(" es incorrecto " + entrada);
-    entrada = prompt("Intenta de nuevo");
+//entrada = prompt ("Jugamos? Adivina el numero del 1 al 10 para poder entrar");
+//while (entrada != "6") {
+//    alert(" es incorrecto " + entrada);
+//    entrada = prompt("Intenta de nuevo");
+//}
+
+let nombre = prompt("Ingresar nombre");
+let edad = prompt("Ingresar edad");
+let email = prompt("Ingresar email");
+let phone = prompt("Ingresar telefono")
+let username = prompt("Ingresar username");
+
+
+function puedeIngresar(edadIng){
+    if(edadIng < 18){ alert("No puede ingresar"); return;}
+}
+puedeIngresar(edad);
+
+
+const validations = (nombre, edad, email, phone, username) =>{
+    let isValidNombre, isValidEdad, isValidEmail, isValidPhone, isValidUsername;
+    if(isNaN(edad) || isNaN(phone)){
+        alert("Por favor revise los datos y vuelva a intentar");
+        let edad = prompt("Ingresar edad");
+        let phone = prompt("Ingresar telefono");
+        return {edad, email};
+    }
+
+    console.log(
+        nombre,
+        edad,
+        email,
+        phone,
+        username
+    );
+    console.log(
+        isValidNombre,
+        isValidEdad,
+        isValidEmail,
+        isValidPhone,
+        isValidUsername
+    );
+   
 }
 
